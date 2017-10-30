ecf252fcd885469cd3f7b399cb05536d7d76fe14
alexolivaresdv <hp.alex.ao@gmail.com>
2017-10-30 18:38:11 -0300

    Encabezado multilinia

---
+++ src/pruebajava1/PruebaJava1.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pruebajava1;
+
+/**************************************************************************
+ * Alex Olivares Del Valle 
+ * @author LAB17
+ * Fecha: Octubre 30 del 2017
+ * Programa: Prueba Java 1
+ * Responsabilidad: Calcular el area de un rectangulo
+ */
+public class PruebaJava1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
